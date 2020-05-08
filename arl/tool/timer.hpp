52d21c115ad00fe0ecce6a80b1e62c14c98ceedc
Jack Yan <jiakunyan1998@gmail.com>
2020-05-08 10:20:30 +0800

    feat(simpletimer): add col_print_{us,s}: this will also print the min/max value across all ranks

--- arl/tool/timer.hpp
+++ arl/tool/timer.hpp
@@ -5,144 +5,136 @@
 #define ARL_TIMER_HPP
 
 namespace arl {
-  // microseconds, 12ns
-  tick_t ticks_now() {
-    return gasneti_ticks_now();
-  }
+template <typename T, typename BinaryOp>
+extern T reduce_all(const T& value, const BinaryOp& op);
+extern rank_t rank_me();
 
-  uint64_t ticks_to_ns(tick_t val) {
-    return gasneti_ticks_to_ns(val);
-  }
+// microseconds, 12ns
+tick_t ticks_now() {
+  return gasneti_ticks_now();
+}
 
-  uint64_t ticks_to_us(tick_t val) {
-    return ticks_to_ns(val) / 1e3;
-  }
+uint64_t ticks_to_ns(tick_t val) {
+  return gasneti_ticks_to_ns(val);
+}
 
-  double ticks_to_ms(tick_t val) {
-    return ticks_to_ns(val) / 1e6;
-  }
+uint64_t ticks_to_us(tick_t val) {
+  return ticks_to_ns(val) / 1e3;
+}
 
-  double ticks_to_s(tick_t val) {
-    return ticks_to_ns(val) / 1e9;
-  }
+double ticks_to_ms(tick_t val) {
+  return ticks_to_ns(val) / 1e6;
+}
 
-  // microseconds, 370ns
+double ticks_to_s(tick_t val) {
+  return ticks_to_ns(val) / 1e9;
+}
+
+// microseconds, 370ns
 //  tick_t ticks_now() {
 //    timespec temp;
 //    clock_gettime(CLOCK_MONOTONIC, &temp);
 //    return temp.tv_sec * 1e6 + temp.tv_nsec / 1e3;
 //  }
 
-  inline void update_average(double &average, uint64_t val, uint64_t num) {
-    average += (val - average) / num;
-//      if (rank_me() == 0) {
-//        std::printf("val=%lu; num=%lu; average=%.2lf\n", val, num, average);
-//      }
-  }
-
-  namespace local {
-    extern rank_t rank_me();
-  }
-
-  struct SharedTimer {
-  private:
-    alignas(alignof_cacheline) unsigned long step = 0;
-    alignas(alignof_cacheline) tick_t _start = 0;
-    alignas(alignof_cacheline) double _ticks = 0;
-  public:
-    void start() {
-      if (local::rank_me() == 0) {
-        _start = ticks_now();
-      } else {
-        ticks_now();
-      }
-    }
-
-    void end() {
-      tick_t _end = ticks_now();
-      if (local::rank_me() == 0) {
-        update_average(_ticks, _end - _start, ++step);
-      }
-    }
-
-    void tick_and_update(tick_t _start_) {
-      tick_t _end = ticks_now();
-      if (local::rank_me() == 0) {
-        update_average(_ticks, _end - _start_, ++step);
-      }
-    }
-
-    [[nodiscard]] double to_ns() const {
-      return ticks_to_ns(_ticks);
-    }
-
-    [[nodiscard]] double to_us() const {
-      return ticks_to_ns(_ticks) / 1e3;
-    }
-
-    [[nodiscard]] double to_s() const {
-      return ticks_to_ns(_ticks) / 1e9;
-    }
-
-    void print_us(std::string &&name = "") const {
-      if (local::rank_me() == 0) {
-        printf("Duration %s: %.3lf us\n", name.c_str(), to_us());
-      }
-    }
-  };
-
-  struct SimpleTimer {
-  public:
-    void start() {
-      _start = ticks_now();
-    }
-
-    void end() {
-      tick_t _end = ticks_now();
-      update_average(_ticks, _end - _start, ++_step);
-    }
-
-    void tick_and_update(tick_t _start_) {
-      tick_t _end = ticks_now();
-      update_average(_ticks, _end - _start_, ++_step);
-    }
-
-    [[nodiscard]] double to_ns() const {
-      return ticks_to_ns(_ticks);
-    }
-
-    [[nodiscard]] double to_us() const {
-      return ticks_to_ns(_ticks) / 1e3;
-    }
-
-    [[nodiscard]] double to_ms() const {
-      return ticks_to_ns(_ticks) / 1e6;
-    }
-
-    [[nodiscard]] double to_s() const {
-      return ticks_to_ns(_ticks) / 1e9;
-    }
-
-    void print_us(std::string &&name = "") const {
-      printf("Duration %s: %.3lf us (step %ld, total %.3lf s)\n", name.c_str(), to_us(), step(), to_s() * step());
-    }
-
-    void print_ms(std::string &&name = "") const {
-      printf("Duration %s: %.3lf ms (step %ld, total %.3lf s)\n", name.c_str(), to_ms(), step(), to_s() * step());
-    }
-
-    void print_s(std::string &&name = "") const {
-      printf("Duration %s: %.3lf s (step %ld, total %.3lf s)\n", name.c_str(), to_s(), step(), to_s() * step());
-    }
-
-    long step() const {
-      return _step;
-    }
-
-   private:
-    long _step = 0;
-    tick_t _start = 0;
-    double _ticks = 0;
-  };
+inline void update_average(double &average, uint64_t val, uint64_t num) {
+  average += (val - average) / num;
 }
+
+namespace local {
+  extern rank_t rank_me();
+}
+
+struct SimpleTimer {
+public:
+  void start() {
+    _start = ticks_now();
+  }
+
+  void end() {
+    tick_t _end = ticks_now();
+    update_average(_ticks, _end - _start, ++_step);
+  }
+
+  void tick_and_update(tick_t _start_) {
+    tick_t _end = ticks_now();
+    update_average(_ticks, _end - _start_, ++_step);
+  }
+
+  [[nodiscard]] double to_ns() const {
+    return ticks_to_ns(_ticks);
+  }
+
+  [[nodiscard]] double to_us() const {
+    return ticks_to_ns(_ticks) / 1e3;
+  }
+
+  [[nodiscard]] double to_ms() const {
+    return ticks_to_ns(_ticks) / 1e6;
+  }
+
+  [[nodiscard]] double to_s() const {
+    return ticks_to_ns(_ticks) / 1e9;
+  }
+
+  void print_us(std::string &&name = "") const {
+    printf("Duration %s: %.3lf us (step %ld, total %.3lf s)\n", name.c_str(), to_us(), step(), to_s() * step());
+  }
+
+  void print_ms(std::string &&name = "") const {
+    printf("Duration %s: %.3lf ms (step %ld, total %.3lf s)\n", name.c_str(), to_ms(), step(), to_s() * step());
+  }
+
+  void print_s(std::string &&name = "") const {
+    printf("Duration %s: %.3lf s (step %ld, total %.3lf s)\n", name.c_str(), to_s(), step(), to_s() * step());
+  }
+
+  SimpleTimer& col_print_us(std::string &&name = "", rank_t r = 0) {
+    double max_duration = reduce_all(to_us(), op_max());
+    double min_duration = reduce_all(to_us(), op_min());
+    int64_t max_step = reduce_all(_step, op_max());
+    int64_t min_step = reduce_all(_step, op_min());
+    double max_total_duration = reduce_all(to_s() * step(), op_max());
+    double min_total_duration = reduce_all(to_s() * step(), op_min());
+    if (rank_me() == r)
+      printf("Duration %s: %.3lf us (step %ld, total %.3lf s) (min/max one %.3f/%.3f step %ld/%ld total %.3f/%.3f)\n",
+             name.c_str(), to_us(), step(), to_s() * step(),
+             min_duration, max_duration,
+             min_step, max_step,
+             min_total_duration, max_total_duration);
+    return *this;
+  }
+
+  SimpleTimer& col_print_s(std::string &&name = "", rank_t r = 0) {
+    double max_duration = reduce_all(to_s(), op_max());
+    double min_duration = reduce_all(to_s(), op_min());
+    int64_t max_step = reduce_all(_step, op_max());
+    int64_t min_step = reduce_all(_step, op_min());
+    double max_total_duration = reduce_all(to_s() * step(), op_max());
+    double min_total_duration = reduce_all(to_s() * step(), op_min());
+    if (rank_me() == r)
+      printf("Duration %s: %.3lf s (step %ld, total %.3lf s) (min/max one %.3f/%.3f step %ld/%ld total %.3f/%.3f)\n",
+          name.c_str(), to_s(), step(), to_s() * step(),
+          min_duration, max_duration,
+          min_step, max_step,
+          min_total_duration, max_total_duration);
+    return *this;
+  }
+
+  long step() const {
+    return _step;
+  }
+
+  void clear() {
+    _step = 0;
+    _start = 0;
+    _ticks = 0;
+  }
+
+ private:
+  int64_t _step = 0;
+  tick_t _start = 0;
+  double _ticks = 0;
+};
+} // namespace arl
 #endif //ARL_TIMER_HPP
