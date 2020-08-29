cfd565066822d08296e365d5e760c9b657a47416
Jiakun Yan <jiakunyan1998@gmail.com>
2020-08-29 11:17:02 +0800

    refactor(dist_wrapper): yes, DistWrapper is just an Allgather

--- arl/data_structure/dist_wrapper.hpp
+++ arl/data_structure/dist_wrapper.hpp
@@ -2,33 +2,33 @@
 #define ARL_DIST_WRAPPER_HPP
 
 namespace arl {
-
-  template <typename T>
-  class DistWrapper {
-  private:
-    std::vector<T *> map_ptrs;
-  public:
-    // initialize the local map
-    template<typename ...U>
-    DistWrapper(T* ptr) {
-      map_ptrs.resize(rank_n());
-      map_ptrs[rank_me()] = ptr;
-      for (size_t i = 0; i < rank_n(); ++i) {
-        broadcast(map_ptrs[i], i);
-      }
+// Actually, it is just an Allgather
+template <typename T>
+class DistWrapper {
+ private:
+  std::vector<T *> map_ptrs;
+ public:
+  // initialize the local map
+  template<typename ...U>
+  DistWrapper(T* ptr) {
+    map_ptrs.resize(rank_n());
+    map_ptrs[rank_me()] = ptr;
+    for (size_t i = 0; i < rank_n(); ++i) {
+      broadcast(map_ptrs[i], i);
     }
+  }
 
-    ~DistWrapper() {
-      // barrier(); // should we explicitly put a barrier in the deconstructor?
-    }
+  ~DistWrapper() {
+    // barrier(); // should we explicitly put a barrier in the deconstructor?
+  }
 
-    T *local() const {
-      return map_ptrs[rank_me()];
-    }
+  T *local() const {
+    return map_ptrs[rank_me()];
+  }
 
-    T *operator[](size_t index) const {
-      return map_ptrs[index];
-    }
+  T *operator[](size_t index) const {
+    return map_ptrs[index];
+  }
 
 //    template<typename RPC_Fn, typename ...Args>
 //    Future <std::invoke_result_t<RPC_Fn, rank_t, T*,  Args...>>
@@ -37,7 +37,7 @@ namespace arl {
 //      return std::invoke(std::forward<RPC_Fn>(rpc_fn), remote_worker, map_ptrs[remote_worker],
 //                         std::forward<Args>(args)...);
 //    }
-  };
+};
 } // class DistWrapper
 
 #endif //ARL_DIST_WRAPPER_HPP
