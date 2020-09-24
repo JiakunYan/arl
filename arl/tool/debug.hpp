3d700e0e995c6ca4a6a0be599f3c6109cbe1f055
Jiakun Yan <jiakunyan1998@gmail.com>
2020-09-24 14:55:55 +0800

    refactor(traceback): slightly change the logic of file passing to the traceback function

--- arl/tool/debug.hpp
+++ arl/tool/debug.hpp
@@ -6,28 +6,29 @@
 #define ARL_TRACEBACK_HPP
 
 namespace arl::debug {
-void arl_traceback() {
+void arl_traceback(FILE *fp) {
   void *array[20];
   size_t size;
   size = backtrace(array, 20);
 
   // print out all the frames to stderr
   if (size != 0) {
-    FILE *fp = fopen(string_format("traceback.arl_dlog.", rank_me()).c_str(), "w");
     int fd;
     if (fp == nullptr) {
       fd = STDERR_FILENO;
     } else {
       fd = fileno(fp);
     }
-    fprintf(fp, "Rank %ld\n", rank_me());
-    fflush(fp);
     backtrace_symbols_fd(array, size, fd);
   }
 }
 
 void default_timeout_handler() {
-  arl::debug::arl_traceback();
+  FILE *fp = fopen(string_format("traceback.arl_dlog.", rank_me()).c_str(), "w");
+  fprintf(fp, "Rank %ld\n", rank_me());
+//  fprintf(fp, "AggBuffer status:\n%s\n", amaggrd_internal::get_amaggrd_buffer_status().c_str());
+  fflush(fp);
+  arl::debug::arl_traceback(fp);
 }
 
 inline void set_timeout(double t) {
